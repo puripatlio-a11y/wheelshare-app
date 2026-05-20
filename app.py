05578da757d5b8e4d9c0724418d7bdc35b9f9495
puripatlio-a11y <puripat.lio@student.mahidol.edu>
2026-05-21 00:49:42 +0700

    app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ header_html = """
 </style>
 
 <div class="custom-header">
-    <h1>♿ AI Accessibility Route Planner (wheelchair)</h1>
+    <h1> AI Accessibility Route Planner (wheelchair)</h1>
     <h3>ระบบวางแผนเส้นทางอัจฉริยะสำหรับผู้ใช้วีลแชร์ </h3>
 </div>
 """
